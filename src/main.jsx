4040c14374de113f482a92b98ad6bd977a9a40b0
Saurabh Khatiwada <khatiwada.saurabh@gmail.com>
2025-08-09 16:00:01 -0400

    WIP

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { SongCompletionProvider } from "./SongCompletionContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
     <SongCompletionProvider>
       <App />
     </SongCompletionProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
